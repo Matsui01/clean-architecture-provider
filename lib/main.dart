d674ff6bb772345b83bc18f015cd34614305c22f
Matsui <paulo.matsui@mail.tantto.com.br>
2023-08-23 19:23:38 -0300

    Substituição do GetIt

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,40 @@
+import 'package:awesome_dio_interceptor/awesome_dio_interceptor.dart';
+import 'package:clean_architecture_provider/src/data/datasource/local/app_database.dart';
+import 'package:clean_architecture_provider/src/domain/repositories/order_repository.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'src/config/router/app_router.dart';
 import 'src/config/themes/app_themes.dart';
-import 'src/domain/repositories/order_repository.dart';
-import 'src/locator.dart';
+import 'src/data/repositories/orders_repository_impl.dart';
+import 'src/utils/constants/strings.dart';
 import 'src/view/providers/orders_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  await initializeDependencies();
+  var dio = Dio();
+  dio.interceptors.add(AwesomeDioInterceptor());
+  AppDatabase db = await $FloorAppDatabase.databaseBuilder(databaseName).build();
+  // await initializeDependencies();
+  // final providers = await getProviders();
   runApp(
     MultiProvider(
       providers: [
+        Provider<Dio>(
+          create: (context) => dio,
+        ),
+        Provider<AppDatabase>(
+          create: (context) => db,
+        ),
+        Provider<OrderRepository>(
+          create: (context) => OrdersRepositoryImpl(
+            context.read<AppDatabase>(),
+          ),
+        ),
         ChangeNotifierProvider(
-          create: (_) => OrdersProvider(
-            getIt<OrderRepository>(),
+          create: (context) => OrdersProvider(
+            context.read<OrderRepository>(),
           ),
         ),
       ],
